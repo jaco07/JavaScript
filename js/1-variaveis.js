d3998287c4deca1587566e223106cb709cd20a1c
jaco07 <115810219+jaco07@users.noreply.github.com>
2023-02-15 18:28:38 -0300

    Add files via upload

---
+++ js/1-variaveis.js
@@ -0,0 +1,8 @@
+var x = 21
+console.log(typeof x)
+
+var nome = "Jacó"
+console.log(typeof nome)
+
+var idade = prompt("Digite sua idade")
+console.log(idade)
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite seu nome")
+var idade = prompt("Digite sua idade")
+
+document.write("" + nome + )
+
